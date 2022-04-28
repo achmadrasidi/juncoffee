1111a8e54f17752994d5c95280cdadb2ad257ec4
kondelow <achmadrasidi@gmail.com>
2022-04-28 09:41:24 +0700

    fix sorted

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,9 +5,9 @@ const usersRouter = require("./userRoutes");
 const promosRouter = require("./promoRoutes");
 const transactionsRouter = require("./transactionRoutes");
 
-Router.use("/product", productsRouter);
 Router.use("/user", usersRouter);
-Router.use("/transaction", transactionsRouter);
+Router.use("/product", productsRouter);
 Router.use("/promo", promosRouter);
+Router.use("/transaction", transactionsRouter);
 
 module.exports = Router;
